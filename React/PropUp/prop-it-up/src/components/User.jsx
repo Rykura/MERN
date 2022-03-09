f61e72ddaf529907d99ee54b859dd6e3715ca8e6
victorsaquet <vs@victorsaquet.com>
2022-03-09 15:33:57 -0500

    state

--- React/PropUp/prop-it-up/src/components/User.jsx
+++ React/PropUp/prop-it-up/src/components/User.jsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react'; //import react and component so that we can use those to create a class component
 
 class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
     render() {
         return(
             <div>
                 <h2>{ this.props.lastName }, { this.props.firstName }</h2>
-                <p>Age: { this.props.age }</p>
+                <p>Age: { this.state.age }</p>
                 <p>Hair Color: { this.props.hairColor }</p>
+                <button onClick= {this.birthday}>Birthday Button!</button>
                 <hr />
             </div>
         )        
+    }
+    birthday = () => {
+        this.setState({count:this.state.age++});
     }
 }
     
