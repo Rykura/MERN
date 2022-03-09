99d9c79916b3320856aef190c70326cba64cac1d
victorsaquet <vs@victorsaquet.com>
2022-03-09 14:22:11 -0500

    update

--- React/FirstJSX/first-react-example/src/App.js
+++ React/FirstJSX/first-react-example/src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1>Hello Dojo!</h1>
       <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
